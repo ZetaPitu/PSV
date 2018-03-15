e37453367d930135bc62c0d585ce6902e96c06f7
ZetaPitu <35515984+ZetaPitu@users.noreply.github.com>
2018-03-15 12:36:39 +0100

    Version 2.0

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Psv = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(231, 59)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(201, 52)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Archivo Excel"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Enabled = False
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(249, 290)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(183, 41)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Descarga lenta"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Psv
+        '
+        Me.Psv.FileName = "Psv.csv"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.LightSlateGray
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Enabled = False
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(231, 164)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(201, 45)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Directorio imagenes"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Yellow
+        Me.Label1.Location = New System.Drawing.Point(227, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Yellow
+        Me.Label2.Location = New System.Drawing.Point(227, 140)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(215, 215)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(231, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(215, 117)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(231, 20)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(12, 241)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(434, 45)
+        Me.TextBox3.TabIndex = 9
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Yellow
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(150, 337)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(156, 37)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Salir  ->Pulse Ctrl"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Teal
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label3.Location = New System.Drawing.Point(47, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(385, 24)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Programa de descarga de logos para IN-Live"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(-1, 398)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox4.Size = New System.Drawing.Size(463, 101)
+        Me.TextBox4.TabIndex = 13
+        Me.TextBox4.Text = resources.GetString("TextBox4.Text")
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.ErrorImage = Global.InLive.My.Resources.Resources.Foto0976
+        Me.PictureBox2.Image = Global.InLive.My.Resources.Resources.cordova
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 36)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(197, 199)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 14
+        Me.PictureBox2.TabStop = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Red
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.Enabled = False
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(24, 290)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(185, 41)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "Descarga rapida"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(-1, 380)
+        Me.ProgressBar1.Maximum = 10000
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(463, 21)
+        Me.ProgressBar1.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Aqua
+        Me.BackgroundImage = Global.InLive.My.Resources.Resources.Chica
+        Me.ClientSize = New System.Drawing.Size(461, 499)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Recuperar imagenes V 1.0"
+        Me.TopMost = True
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Psv As OpenFileDialog
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,322 @@
+﻿Imports System.IO
+Imports System.Windows.Markup
+Imports MS.Internal
+
+Public Class Form1
+    Private contador As Int32 = 0
+    Private contador1 As Int32 = 0
+    Private contador2 As Drawing.Size
+    Private Errores As Int32 = 0
+    Private Final As Int32
+    Private myStream As Stream = Nothing
+    Private DirPsv = "http://sophrologie-dynamique.weebly.com/uploads/1/1/3/2/11325139/1334317102.jpg "
+    Private img = "1.jpg"
+    Private DirImg = Nothing
+    Private ArchivoWeb(2000) As String
+    Private ArchivoPc(2000) As String
+    Private Rapido As Boolean = True
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Rapido = True
+        Try
+
+            ReadFile()
+
+        Catch Ex As Exception
+
+            MessageBox.Show(Ex.Message)
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        myStream = Nothing
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        REM openFileDialog1.InitialDirectory = "Y:\"
+        openFileDialog1.Filter = "Excel separado por ; (*.csv) |*.csv|Todos los archivos|*.*"
+        openFileDialog1.FilterIndex = 1
+        openFileDialog1.RestoreDirectory = True
+        TextBox2.Text = openFileDialog1.FileName
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Try
+
+                TextBox2.Text = openFileDialog1.FileName
+                Button2.Enabled = False
+                Button3.Enabled = True
+
+                REM  Leer_Archivo_Excel()
+
+            Catch Ex As Exception
+                MessageBox.Show("Error al abrir archivo: " & Ex.Message)
+            Finally
+                ' Check this again, since we need to make sure we didn't throw an exception on open.
+                If (myStream IsNot Nothing) Then
+                    myStream.Close()
+                End If
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        REM FolderBrowserDialog1.SelectedPath = "Y:\"
+
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+            Button3.Enabled = False
+            Button1.Enabled = True
+            Button5.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = " Archivo de excel"
+        Label2.Text = " Directorio de destino"
+        FolderBrowserDialog1.ShowDialog()
+    End Sub
+    Function Leer_Archivo_Excel() As Boolean
+
+
+
+        ReadFile()
+
+        REM MessageBox.Show(TextBox3.Text)
+        Return True
+    End Function
+
+
+    Function Leer_Archivo(Web As String, Local As String) As Boolean
+        Try
+            TextBox3.Text = Web
+            REM   My.Computer.Network.DownloadFile(Web, Local, "anonymous", "")
+            ArchivoWeb(contador) = New String(Web)
+            ArchivoPc(contador) = Local
+            contador = contador + 1
+            If contador > 1000 Then
+                REM   MsgBox(Web)
+            End If
+            TextBox4.Text = TextBox4.Text + " : " + Web + vbCrLf
+        Catch ex As Exception
+            MsgBox("Error " & ex.Message & " Error de escritura ")
+        End Try
+        Return True
+    End Function
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Timer1.Enabled = False
+
+        Dim response = MsgBox("Finalizar", MsgBoxStyle.YesNo, "In Live")
+        If response = MsgBoxResult.Yes Then
+            Application.Exit()
+        End If
+
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub ReadFile()
+        Label3.Text = "Leyendo logos a ......Espere"
+        Button5.Enabled = False
+        Button1.Enabled = False
+        Using MyReader As New Microsoft.VisualBasic.
+            FileIO.TextFieldParser(TextBox2.Text, System.Text.Encoding.Unicode)
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(";; ")
+            Dim currentRow As String()
+            Try
+                currentRow = MyReader.ReadFields()
+            Catch ex As Microsoft.VisualBasic.
+                            FileIO.MalformedLineException
+                MsgBox("Line " & ex.Message &
+                    " Error en linea ")
+            End Try
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+                    Dim currentField As String
+                    For Each currentField In currentRow
+                        My.Application.DoEvents()
+                        If (currentField.Length > 3) Then
+                            TextBox3.Text = currentField
+                            Label3.Text = "Leyendo logos a ...... " + CType(contador, String)
+                            Crear_Archivo(TextBox3.Text)
+                            Try
+                                ProgressBar1.Maximum = contador + 1
+                                ProgressBar1.Value = contador
+                            Catch ex As Exception
+                                ProgressBar1.Value = 0
+                            End Try
+
+
+                        End If
+
+                    Next
+                Catch ex As Microsoft.VisualBasic.
+                            FileIO.MalformedLineException
+                    MsgBox("Line " & ex.Message &
+                    "Error en linea")
+                End Try
+
+            End While
+
+            Descargar_Logos()
+        End Using
+    End Sub
+    Private Sub ReadLine()
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(TextBox2.Text)
+
+            MyReader.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(";;")
+            Dim currentRow As String()
+            'Loop through all of the fields in the file. 
+            'If any lines are corrupt, report an error and continue parsing. 
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+                    MsgBox(currentRow)
+                Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                    MsgBox("Line " & ex.Message &
+                    " is invalid.  Skipping")
+                End Try
+            End While
+        End Using
+    End Sub
+
+    Private Sub Crear_Archivo(S As String)
+
+        Dim TestString As String = S
+        Dim TestArray() As String = Split(TestString, ";")
+        ' TestArray holds {"apple", "", "", "", "pear", "banana", "", ""}
+        Dim LastNonEmpty As Integer = -1
+        For i As Integer = 0 To TestArray.Length - 1
+            If TestArray(i) <> ";" Then
+                LastNonEmpty += 1
+                TestArray(LastNonEmpty) = TestArray(i)
+            End If
+        Next
+        ReDim Preserve TestArray(LastNonEmpty)
+        ' TestArray now holds {"apple", "pear", "banana"}
+
+        TextBox4.Text = TestArray(1) & "  ---> " & TextBox1.Text & "\" & TestArray(4) & ".inlive"
+        Leer_Archivo(TestArray(1), TextBox1.Text & "\" & TestArray(4) & ".inlive")
+        REM Logo.Url = New Uri(TestArray(1))
+    End Sub
+    Private Sub Descargar_Logos()
+        Dim control As Boolean = True
+        MsgBox("Logos --> " + CType(contador, String))
+        ProgressBar1.Value = 0
+        ProgressBar1.Maximum = contador + 1
+
+        Label3.Text = "Descargando logos  ......Espere"
+        For x = 0 To contador
+            contador1 = x
+
+            My.Application.DoEvents()
+            Try
+                Try
+                    ProgressBar1.Value = ProgressBar1.Value + 1
+                Catch ex As Exception
+                    ProgressBar1.Value = 0
+                End Try
+
+                TextBox3.Text = ArchivoWeb(x)
+                TextBox4.Text = "Registro -->" + CType(x + 1, String) + " --->" + ArchivoWeb(x) + " -->" + ArchivoPc(x) + vbCrLf
+                Label3.Text = "Descargando logos  ...... " + CType(x, String) + " Errores -> " + CType(Errores, String)
+                My.Computer.Network.DownloadFile(ArchivoWeb(x), ArchivoPc(x), "anonymous", "")
+                VerImagen(ArchivoPc(x))
+            Catch ex As Exception
+                Try
+                    VerImagen(ArchivoPc(x))
+                    TextBox4.Text = TextBox4.Text + "Error -->" + ex.Message
+
+                Catch ei As Exception
+                    PictureBox2.Image = InLive.My.Resources.Resources.Foto0976
+                    TextBox4.Text = TextBox4.Text + "Error -->" + ei.Message
+                    Errores += 1
+                End Try
+
+            End Try
+            If (Rapido) Then
+                Dim response = MsgBox("Siguiente", MsgBoxStyle.OkCancel, "In Live")
+                If response = MsgBoxResult.Cancel Then
+                    Application.Exit()
+                    x = contador
+
+                End If
+            End If
+        Next
+
+        PictureBox2.Image = InLive.My.Resources.Resources.cordova
+            Label3.Text = "Descarga completada  Errores -> " + CType(Errores, String)
+            TextBox4.Text = "                 Descarga completada" + vbLf + vbLf + "                 Pulse Salir para cerrar el programa"
+
+
+
+    End Sub
+    Public Sub VerImagen(fileToDisplay As String)
+        ' Sets up an image object to be displayed.
+        Dim MyImage As Bitmap = Nothing
+        If (MyImage IsNot Nothing) Then
+            MyImage.Dispose()
+        End If
+
+        ' Stretches the image to fit the pictureBox. 
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        MyImage = New Bitmap(fileToDisplay)
+        PictureBox2.Image = CType(MyImage, Image)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        REM TextBox4.Text = ArchivoWeb(contador1) + vbCrLf
+        If My.Computer.Keyboard.CtrlKeyDown Then
+            Timer1.Enabled = False
+
+            Dim response = MsgBox("Finalizar", MsgBoxStyle.YesNo, "In Live")
+            If response = MsgBoxResult.Yes Then
+                Application.Exit()
+            End If
+
+            Timer1.Enabled = True
+
+        Else
+            REM  MsgBox("CTRL key up")
+        End If
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Rapido = False
+        Try
+
+
+            ReadFile()
+
+        Catch Ex As Exception
+
+            MessageBox.Show(Ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
